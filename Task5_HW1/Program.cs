35e3008c32658fc43c24cffd2322896a7cfaea4f
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-26 19:45:03 +0500

    add Task5_HW1

---
+++ Task5_HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число А => ");                              
+string A = Console.ReadLine();                            
+int numberA = int.Parse(A);     
+Console.Write("Введите число B => ");                              
+string B = Console.ReadLine();                            
+int numberB = int.Parse(B);     
+if (numberA == numberB)
+{
+   Console.WriteLine("а равно b");
+}
+else 
+  if (numberA<numberB)
+  {
+    Console.WriteLine("b большее");
+    Console.WriteLine("a меньшее");
+  }
+  else
+  { 
+    Console.WriteLine("a большее");
+    Console.WriteLine("b меньшее");
+  }
