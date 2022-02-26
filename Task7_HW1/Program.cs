c4b175e2a9b6b63d97fc8babad088d63e5b5fbe6
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-26 22:41:22 +0500

    add Task7_HW1

---
+++ Task7_HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число А => ");                              
+string A = Console.ReadLine();                            
+int a = int.Parse(A); 
+if (a%2==0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("не четное");
+}
