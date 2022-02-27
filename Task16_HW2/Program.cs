d9ecdd7fbcd85f4f3afa779988dab474cf24953c
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-27 14:58:04 +0500

    add Task16_HW2

---
+++ Task16_HW2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите номер дня недели => ");                              
+string A = Console.ReadLine();                            
+int a = int.Parse(A);
+if (a<8&&a>0)
+{
+    if (a==6||a==7) Console.WriteLine("Выходной день!");
+    else Console.WriteLine("Рабочий день");
+}    
+else Console.WriteLine("Только 7 дней в неделе!");
