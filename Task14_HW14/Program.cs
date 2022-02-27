5417da862aac54edf6a6a0d0427d6a4fcf4d663d
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-28 00:38:20 +0500

    add Task14_HW2

---
+++ Task14_HW14/Program.cs
@@ -0,0 +1,8 @@
+﻿Random rnd=new Random();
+int value=rnd.Next(100, 999);
+Console.WriteLine($"Случайное трехзначное число {value}");
+int number1=value/100%10;
+int number2=value%10;
+Console.Write("Число без второй цифры ");
+Console.Write(number1);
+Console.Write(number2);
