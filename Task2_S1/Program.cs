8971657c7233c88502319f2fdbff06267c486e1b
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-27 09:32:02 +0500

    add Task2_S1

---
+++ Task2_S1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите номер дня недели => ");                              
+string A = Console.ReadLine();                            
+int a = int.Parse(A); 
+if (a<8&&a>0)
+{
+    if (a==1) Console.WriteLine("Понедельник!");
+    if (a==2) Console.WriteLine("Вторник!");
+    if (a==3) Console.WriteLine("Среда!");
+    if (a==4) Console.WriteLine("Четверг!");
+    if (a==5) Console.WriteLine("Пятница!");
+    if (a==6) Console.WriteLine("Суббота!");
+    if (a==7) Console.WriteLine("Воскресенье!");
+    
+}
+else
+{
+    Console.WriteLine("Только 7 дней в неделе!");
+}
