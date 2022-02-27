5fe8e106264ecfd1110576bc553f601aa58edeba
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-02-27 23:57:05 +0500

    add Task13_HW2

---
+++ Task13_HW2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число => ");                              
+string A = Console.ReadLine();                            
+int a = int.Parse(A);
+if (a<=999&&a>=100) Console.WriteLine($"Вторая цифра трехзначного числа равна {a/10%10}");
+else Console.WriteLine($"Введено не трехзначное число");
