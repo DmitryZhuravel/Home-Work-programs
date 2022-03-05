1a6b7e376a18956b6cfe0e3f6a7dee5620f9420e
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-05 15:51:59 +0500

    add Task15_HW2_1

---
+++ Task15_HW2_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число => ");
+string A = Console.ReadLine();
+int[] arr = new int[A.Length];
+for (int i = 0; i < A.Length; i++)
+{
+    arr[i] = Convert.ToInt32(A.Substring(i, 1));
+}
+if (arr.Length < 3) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine($"Третья цифра заданного числа => {arr[2]}");
