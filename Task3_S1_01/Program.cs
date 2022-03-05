999540a4bb806a2263d680f40a34d01d590a5890
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-05 23:53:33 +0500

    add Task3_S1_01

---
+++ Task3_S1_01/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число x => ");                              
+string x = Console.ReadLine();                            
+int X = int.Parse(x);        
+int y = X*X + 1; 
+Console.WriteLine($"Функция y = f(x): {y}");
