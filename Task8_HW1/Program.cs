a83b3c16892dbc24aaa0672cb92703da07f3a687
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-05 19:37:59 +0500

    add Task8_HW1_use i=2

--- Task8_HW1/Program.cs
+++ Task8_HW1/Program.cs
@@ -1,10 +1,17 @@
 ﻿Console.Write("Введите число => ");
 string inputN = Console.ReadLine();
 int N = int.Parse(inputN);
-int i=1;
+int i=2;
 while (i<N)
 {
-  if (i%2==0) 
   Console.Write(i+" ");
-  i=i+1;
+  i=i+2;
 }
+
+//int i=1;
+//while (i<N)
+//{
+//  if (i%2==0) 
+//Console.Write(i+" ");
+//i=i+1;
+//}
