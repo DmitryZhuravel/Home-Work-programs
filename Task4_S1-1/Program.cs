36e064c348ceaae1104a8d98d0ecad03e1225604
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-06 01:32:10 +0500

    add Task4_S1-1

---
+++ Task4_S1-1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число N => ");                              
+string n = Console.ReadLine();                            
+int N = int.Parse(n);   
+if (N<0) Console.Write($"Модуль числа равен {N=-N}");   
+else Console.Write($"Модуль числа равен {N}"); 
