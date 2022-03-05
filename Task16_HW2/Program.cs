d08fd23a8a6bdb989da9d462edea88e42fffc5e1
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-05 19:30:38 +0500

    add Task6_HW1 del_string

--- Task16_HW2/Program.cs
+++ Task16_HW2/Program.cs
@@ -1,9 +1,16 @@
 ﻿Console.Write("Введите номер дня недели => ");                              
 string A = Console.ReadLine();                            
 int a = int.Parse(A);
-if (a<8&&a>0)
+if (a>0&&a<8)
 {
-    if (a==6||a==7) Console.WriteLine("Выходной день!");
+    if (a>5&&a<8) Console.WriteLine("Выходной день!");
     else Console.WriteLine("Рабочий день");
 }    
 else Console.WriteLine("Только 7 дней в неделе!");
+
+// if (a>0&&a<8)
+//{
+//    if (a==6||a==7) Console.WriteLine("Выходной день!");
+//    else Console.WriteLine("Рабочий день");
+//}    
+// else Console.WriteLine("Только 7 дней в неделе!");
--- Task6_HW1/Program.cs
+++ Task6_HW1/Program.cs
@@ -8,7 +8,6 @@ Console.Write("Введите число C => ");
 string C = Console.ReadLine();                            
 int c = int.Parse(C);     
 int max=a;
-if(a>max) max=a;
 if(b>max) max=b;
 if(c>max) max=c;
 Console.Write("Максимальное число = ");
