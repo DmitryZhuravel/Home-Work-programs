bfd3cccb067cea5a10525fe2bfc7c8f993551a0a
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-06 01:41:57 +0500

    add Task4_S1-2

---
+++ Task4_S1-2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число N => ");                              
+string n = Console.ReadLine();                            
+double N = double.Parse(n);   
+Console.Write($"Обратное значение равно {1/N}");   
