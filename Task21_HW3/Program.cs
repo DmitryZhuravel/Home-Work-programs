4a753a26c99b239a18d8bf81d604c230c0a8df08
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-06 17:40:51 +0500

    add Task21 HW3

---
+++ Task21_HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3D пространстве.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int x1=Prompt("Введите значение координаты x1 =>  ");
+int y1=Prompt("Введите значение координаты y1 =>  ");
+int z1=Prompt("Введите значение координаты z1 =>  ");
+int x2=Prompt("Введите значение координаты x2 =>  ");
+int y2=Prompt("Введите значение координаты y2 =>  ");
+int z2=Prompt("Введите значение координаты z2 =>  ");
+
+int x=x2-x1; 
+int y=y2-y1;
+int z=z2-z1;
+double distance=Math.Sqrt(x*x+y*y+z*z);
+
+Console.Write($"Расстояние между двуми точками в пространстве равно {distance}");
+
