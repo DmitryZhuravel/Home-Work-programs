a90bc3b484065daa1bdfefab4a37475e93e86d98
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-06 18:39:26 +0500

    add Task22 HW3

---
+++ Task22_HW3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int a=Prompt("Введите пятизначное число =>  ");
+if (a > 9999 && a < 100000)
+{
+   int b=a/10000; 
+   int c=a%10000/1000; 
+   int d=a/10%10; 
+   int e=a%10; 
+   if (b == e && c == d) Console.Write("Это число палиндром");
+   else Console.Write("Это число не палиндром");
+}
+else Console.Write("Вы ввели не пятизначное число!!!");
