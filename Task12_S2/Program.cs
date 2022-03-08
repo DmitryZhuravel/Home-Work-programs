dda6dcc187cc355fcf33c2a67a5e2b820cc4e73e
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-08 17:41:46 +0500

    add Task12_S2

---
+++ Task12_S2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход//число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int a=Prompt("Введите число a =>  ");
+if (a%7==0&&a%23==0) Console.WriteLine($"Число {a} кратно одновременно 7 и 23");
+if (a%7==0&&a%23!=0) Console.WriteLine($"Число {a} кратно 7 и не кратно 23");
+if (a%7!=0&&a%23==0) Console.WriteLine($"Число {a} не кратно 7 и кратно 23");
+if (a%7!=0&&a%23!=0) Console.WriteLine($"Число {a} не кратно 7 и не кратно 23");
+
