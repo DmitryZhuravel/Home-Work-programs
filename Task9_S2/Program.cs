2d67fe73074b031ed8f01fa2551bacfabea1136a
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-08 15:18:53 +0500

    add Task9_S2

---
+++ Task9_S2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int a=Prompt("Введите значение координаты a =>  ");
+if (a<=1000&&a>=100)
+{
+    Console.WriteLine($"Последняя цифра трехзначного числа равна {a%10}");
+}
+else Console.WriteLine("Введено не трехзначное число");
