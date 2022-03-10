4f08ad0e2484c749479ec94b73596cb1b8299ffa
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-10 12:06:34 +0500

    add Task19_S3

---
+++ Task19_S3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу квадратов чисел от 1 до N.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int n=Prompt("Введите число N =>  ");
+for (int i=1; i<=n; i++) Console.Write(i*i+" ");
+
