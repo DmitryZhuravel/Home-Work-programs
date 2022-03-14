cf8642e74e9b4fba16edb85b1359eff7527390c2
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-14 18:34:15 +0500

    add Task23 HW3

--- Task23_HW3/Program.cs
+++ Task23_HW3/Program.cs
@@ -1,23 +1,20 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-// является ли оно палиндромом.
-////int Prompt(string message)
-//{
- //   Console.Write(message);
- //   string strInput=Console.ReadLine();
- //   int answer=int.Parse(strInput);
- //   return answer;
-//}
-//int a=Prompt("Введите пятизначное число =>  ");
-//if (a > 9999 && a < 100000)
-//{
-//    int b=a/10000; 
-//    int c=a%10000/1000; 
-///    int d=a/10%10; 
- //   int e=a%10; 
- //   if (b == e && c == d) Console.WriteLine("Это число палиндром");
- //   else Console.WriteLine("Это число не палиндром");
-//}
-//else Console.WriteLine("Вы ввели не пятизначное число!!!");
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Решение через реверс
+Console.Write("Введите трехзначное число => ");                              
+string B = Console.ReadLine();                            
+int a = int.Parse(B);
+string s = a.ToString();
+int S = int.Parse(s);
+char[] ar = s.ToCharArray();
+Array.Reverse(ar);
+s = new String(ar);
+a = Convert.ToInt32(s);
+if (a==S) Console.WriteLine($"Число {a} является полиндром");
+else Console.WriteLine($"Число {B} не является полиндромом, после разворота оно равно {s}");
+
+
+
 // Решение через массив
 Console.Write("Введите число => ");
 string A = Console.ReadLine();
@@ -46,26 +43,26 @@ if (arr.Length==5)
 }
 if (arr.Length==6) 
 {
-    if (arr[0]==arr[5]&&arr[1]==arr[4]) Console.WriteLine("Это число палиндром");
+    if (arr[0]==arr[5]&&arr[1]==arr[4]&&arr[2]==arr[3]) Console.WriteLine("Это число палиндром");
     else Console.WriteLine("Это число не палиндром");
 }
 if (arr.Length==7) 
 {
-    if (arr[0]==arr[6]&&arr[1]==arr[5]) Console.WriteLine("Это число палиндром");
+    if (arr[0]==arr[6]&&arr[1]==arr[5]&&arr[2]==arr[4]) Console.WriteLine("Это число палиндром");
     else Console.WriteLine("Это число не палиндром");
 }    
 if (arr.Length==8)
 {
-    if (arr[0]==arr[7]&&arr[1]==arr[6]) Console.WriteLine("Это число палиндром");
+    if (arr[0]==arr[7]&&arr[1]==arr[6]&&arr[2]==arr[5]&&arr[3]==arr[4]) Console.WriteLine("Это число палиндром");
     else Console.WriteLine("Это число не палиндром");
 }
 if (arr.Length==9) 
 {
-    if (arr[0]==arr[8]&&arr[1]==arr[7]) Console.WriteLine("Это число палиндром");
+    if (arr[0]==arr[8]&&arr[1]==arr[7]&&arr[2]==arr[6]&&arr[3]==arr[5]) Console.WriteLine("Это число палиндром");
     else Console.WriteLine("Это число не палиндром");
 }
 if (arr.Length==10) 
 {
-    if (arr[0]==arr[9]&&arr[1]==arr[8]) Console.WriteLine("Это число палиндром");
+    if (arr[0]==arr[9]&&arr[1]==arr[8]&&arr[2]==arr[7]&&arr[3]==arr[6]) Console.WriteLine("Это число палиндром");
     else Console.WriteLine("Это число не палиндром");
 }
