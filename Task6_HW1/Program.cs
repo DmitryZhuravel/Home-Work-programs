9c007e2a981a9e57023f51e9a48d78ba5843272f
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-16 13:04:29 +0500

    add Task6_HW1

--- Task6_HW1/Program.cs
+++ Task6_HW1/Program.cs
@@ -10,5 +10,19 @@ int c = int.Parse(C);
 int max=a;
 if(b>max) max=b;
 if(c>max) max=c;
-Console.Write("Максимальное число = ");
-Console.WriteLine(max);
+Console.WriteLine($"Максимальное число ={max}");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput=Console.ReadLine();
+    int answer=int.Parse(strInput);
+    return answer;
+}
+int d=Prompt("Введите число a =>  ");
+int f=Prompt("Введите число b =>  ");
+int j=Prompt("Введите число c =>  ");
+int maxx=a;
+if (f>maxx) maxx=f;
+if (j>maxx) maxx=j;
+Console.WriteLine($"Максимальное число ={maxx}");
